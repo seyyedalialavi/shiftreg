3e2080a67c86419d262ce0265f76495a08432e2d
Ali Alavi <ali1500ala@gmail.com>
2020-10-27 13:55:58 +0330

    new commit shift-reg

--- SHIFTR-L.v
+++ SHIFTR-L.v
@@ -1,1 +1,13 @@
-
+module ShiftReg( Clock,L, RandL,D,Y,Q);
+	input wire Clock,L,RandL;	
+	input wire [3:0] D;	
+	output wire [3:0] Q;
+	reg [3:0] Y;
+	
+	always@(posedge Clock or posedge L)
+	begin
+		
+		
+		assign Y = L ? ({D[3], D[2], D[1], D[0]}):(RandL ? ({Q[2], Q[1], Q[0], D[0]}):({D[3], Q[3], Q[2], Q[1]}))
+		
+	end
--- dflop.v
+++ dflop.v
@@ -1,1 +1,6 @@
-
+module flipflop(D,Clock,Q);
+	input D,Clock;
+	output reg Q;
+	always @ (posedge Clock)
+		Q=D;
+endmodule
